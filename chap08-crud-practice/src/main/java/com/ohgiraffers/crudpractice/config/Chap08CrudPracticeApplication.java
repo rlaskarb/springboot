6bbbf25862974f3df15601e6dce0bf20a368870e
rlaskarb <rlaskarb20@gmail.com>
2024-11-28 22:56:23 +0900

    11/28

--- chap08-crud-practice/src/main/java/com/ohgiraffers/crudpractice/config/Chap08CrudPracticeApplication.java
+++ chap08-crud-practice/src/main/java/com/ohgiraffers/crudpractice/config/Chap08CrudPracticeApplication.java
@@ -3,7 +3,8 @@ package com.ohgiraffers.crudpractice.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication //spring
 public class Chap08CrudPracticeApplication {
 
     public static void main(String[] args) {
